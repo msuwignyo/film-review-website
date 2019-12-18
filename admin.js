62d3e11e3b78fa27c84dcf3e7ec79cbc388981a1
m4rk00s <marktony.x3.24@gmail.com>
2019-12-18 12:22:21 +0700

    CRUD Film selesai

---
+++ admin.js
@@ -0,0 +1,45 @@
+const FilmController = require('./controllers/index').FilmController
+
+/* DRIVER CODE -------------------------------------------------------------- */
+
+/* ADD ONE FILM ------------------------------------------------------------- */
+
+// FilmController.addFilm({
+//   name: `Yesterday`,
+//   poster: null,
+//   description: `Jack Malik is a struggling singer-songwriter in an English seaside town whose dreams of fame are rapidly fading, despite the fierce devotion and support of his childhood best friend, Ellie. After a freak bus accident during a mysterious global blackout, Jack wakes up to discover that The Beatles have never existed. Performing songs by the greatest band in history to a world that has never heard them, Jack becomes on overnight sensation with a little help from his agent.`,
+//   trailer: `https://youtu.be/qD6FDkUXSZQ`
+// });
+
+// FilmController.addFilm({
+//   name: `Skyfall`,
+//   poster: null,
+//   description: `An ex-MI6 agent steals a hard drive with top secret information to carry out a vendetta on Bond's overseer, M. Bond must face his past in a bid to try and save M.`,
+//   trailer: `https://youtu.be/sE7_WdlYGJI`
+// });
+
+// FilmController.addFilm({
+//   name: `Aquaman`,
+//   poster: null,
+//   description: `Half-human, half-Atlantean Arthur is born with the ability to communicate with marine creatures. His motive is to retrieve the legendary Trident of Atlan and protect the water world.`,
+//   trailer: `https://youtu.be/WDkg3h8PCVU`
+// });
+
+/* SHOW ALL FILM ------------------------------------------------------------ */
+
+// FilmController.showAllFilms();
+
+/* SHOW ONE FILM ------------------------------------------------------------ */
+
+// FilmController.showOneFilm(2);
+
+/* UPDATE FILM -------------------------------------------------------------- */
+
+// FilmController.updateFilm(1, {
+//   name: 'Yesteryear',
+//   trailer: 'https://youtu.be/Fha2FtS8wJA'
+// });
+
+/* DELETE FILM -------------------------------------------------------------- */
+
+FilmController.deleteFilm(1);
--- migrations/20191217235922-create-film.js
+++ migrations/20191217235922-create-film.js
@@ -15,7 +15,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(1500)
       },
       trailer: {
         type: Sequelize.STRING
